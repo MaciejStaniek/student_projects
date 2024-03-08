ef8778e785317120398695a63a269369fd510c07
MaciejStaniek <mstaniek_211i2@e.zseeim.edu.pl>
2024-03-08 11:19:58 +0000

    studentmachine send

--- cpp/szablon/main.cpp
+++ cpp/szablon/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+int x;
 int main(){
-	cout<<"witaj swiecie"<<endl;
+	string przedmiot;
+	cout<<"podaj wartosc, moze ona wynosic K,N lub P"<<endl;
+	cin>>przedmiot;
+	while(przedmiot!="K"&&"N"&&"P"){
+	cout<<"Bledna wartosc wpisz jeszcze raz :";
+	cin>>przedmiot;
+}
+	if(przedmiot=="K"&&"N"&&"P"){
+	srand(time(0));
+	int x= rand() %4;
+	cout<<x;
+}
 	return 0;
 }
