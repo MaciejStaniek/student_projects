873036360eced0d04c205aec87ee5714e5827ac9
MaciejStaniek <mstaniek_211i2@e.zseeim.edu.pl>
2024-03-15 11:07:57 +0000

    wyslanie pracy

---
+++ python/cwiczenie1/main.py
@@ -0,0 +1,9 @@
+x=0
+x=int(input("podaj"))
+if x>6:
+    print("liczba jest wieksza od 6")
+if x<6:
+    print("liczba jest mniejsza od 6")
+if x==6:
+    print("liczba jest rowna 6")
+print ("witaj swiecie")
