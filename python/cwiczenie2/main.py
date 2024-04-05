82a42fd97794c3350c7c8d383fc6fd3be2566277
MaciejStaniek <mstaniek_211i2@e.edu.pl>
2024-04-05 10:19:13 +0000

    studentmachine send

---
+++ python/cwiczenie2/main.py
@@ -0,0 +1,13 @@
+licznik=0
+imie=0
+
+while licznik < 3:
+    print("jak się nazywasz?")
+    imie = input("nazywam sie ")
+
+    licznik += 1
+
+print("Witaj",(imie))
+
+#    if odpowiedz.lower() != 'tak':
+#	break
